660361cfa0a2fa66c3a6ab4cb1ada0667c4adc86
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2025-11-06 13:36:21 +0900

    [level 0] Title: 배열 만들기 2, Time: 88.01 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,13 @@
+function solution(l, r) {
+    let answer = [];
+    
+    for (let num = l; num <= r; num++) {
+        const strNum = String(num);
+        
+        if (!/[^05]/.test(strNum)) {
+            answer.push(num);
+        }
+    }
+    
+    return answer.length > 0 ? answer : [-1];
+}

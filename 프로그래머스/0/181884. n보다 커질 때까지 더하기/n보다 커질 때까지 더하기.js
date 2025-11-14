07f9dbe20bb5ccea113e80c114d987af1a9da965
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2025-11-14 11:09:29 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers, n) {
+    let answer = 0;
+    let index = 0;
+    
+    while(answer <= n) {
+        answer += numbers[index];
+        index++;
+    }
+    
+    return answer;
+}

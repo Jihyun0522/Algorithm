ec939220109901977131a3b358f22fc317804dfc
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2025-11-19 22:50:37 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,11 @@
+const ops = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+function solution(binomial) {
+    const [a, op, b] = binomial.split(' ');
+    
+    return ops[op](+a, +b);
+}

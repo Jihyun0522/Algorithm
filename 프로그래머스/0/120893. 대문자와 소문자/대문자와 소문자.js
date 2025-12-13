cae25068c92aa9039227a03fd8f551010096672d
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2025-12-13 22:25:31 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,6 @@
+function solution(my_string) {
+    return my_string.split('').map(char => {
+        if (char === char.toUpperCase()) return char.toLowerCase();
+        else return char.toUpperCase();
+    }).join('');
+}

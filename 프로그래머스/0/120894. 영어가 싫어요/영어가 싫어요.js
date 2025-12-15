7a29a8417428f458ce0deef5e008261ddb6a9811
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2025-12-15 19:47:31 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+    const mappingDict = {
+        "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+        "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"
+    };
+    
+    for (const eng in mappingDict){
+        const char = mappingDict[eng];
+        numbers = numbers.replaceAll(eng, char);
+    }
+    
+    return parseInt(numbers);
+}

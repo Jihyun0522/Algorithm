cdc31f71642582fe6e2bd7c93afef76d310807a4
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2026-01-25 23:39:44 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.98 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = []
+    last_pos = {}
+    
+    for i, char in enumerate(s):
+        if char in last_pos:
+            answer.append(i - last_pos[char])
+        else:
+            answer.append(-1)
+            
+        last_pos[char] = i
+        
+    return answer

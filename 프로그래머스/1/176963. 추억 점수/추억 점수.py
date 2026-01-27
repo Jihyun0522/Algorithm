81fc109439167262ac1fb13d0616c93a2a835d1a
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2026-01-27 22:45:12 +0900

    [level 1] Title: 추억 점수, Time: 1.90 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,11 @@
+def solution(name, yearning, photo):
+    answer = []
+    score_dict = dict(zip(name, yearning))
+    
+    for p in photo:
+        score = 0
+        for person in p:
+            score += score_dict.get(person, 0)
+        answer.append(score)
+    
+    return answer

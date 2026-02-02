a18a2940925cd809065545493e87f6234947547c
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2026-02-02 23:34:22 +0900

    [level 1] Title: 소수 만들기, Time: 29.07 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,19 @@
+def solution(nums):
+    answer = 0
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                total = nums[i] + nums[j] + nums[k]
+                
+                if is_prime(total):
+                    answer += 1
+
+    return answer
+
+def is_prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+            
+    return True

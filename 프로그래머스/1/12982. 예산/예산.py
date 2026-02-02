6905c8c91062c3d593f5e583605ff272c5ef202a
Jihyun <42933755+Jihyun0522@users.noreply.github.com>
2026-02-02 23:24:22 +0900

    [level 1] Title: 예산, Time: 0.02 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -0,0 +1,12 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    
+    for amount in d:
+        if budget >= amount:
+            budget -= amount
+            answer += 1
+        else:
+            break
+            
+    return answer

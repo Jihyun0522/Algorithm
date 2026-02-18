16be1db3b8e92036c3a27fc581b55117eab84247
Jihyun <wnlemforhs24@gmail.com>
2026-02-18 23:38:41 +0900

    [Bronze III] Title: 알람 시계, Time: 92 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync(0).toString().trim().split(' ');
+
+let H = parseInt(input[0]);
+let M = parseInt(input[1]);
+
+M -= 45;
+
+if (M < 0) {
+    M += 60;
+    H -= 1;
+    
+    if (H < 0) {
+        H = 23;
+    }
+}
+
+console.log(H, M);

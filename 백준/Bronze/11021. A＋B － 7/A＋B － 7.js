5f8493f3b19c6a413bb527da178d4c7227971cab
Jihyun <wnlemforhs24@gmail.com>
2026-02-20 00:18:40 +0900

    [Bronze V] Title: A+B - 7, Time: 84 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+const input = require('fs').readFileSync(0).toString().split('\n');
+
+const T = parseInt(input[0]);
+let result = "";
+
+for (let i = 1; i <= T; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    
+    result += `Case #${i}: ${A + B}\n`;
+}
+
+process.stdout.write(result);

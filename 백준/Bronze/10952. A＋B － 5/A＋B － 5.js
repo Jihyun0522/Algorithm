b10631ec356acbd81df6d73f83aa914b8254c74b
Jihyun <wnlemforhs24@gmail.com>
2026-02-24 14:20:05 +0900

    [Bronze V] Title: A+B - 5, Time: 100 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,11 @@
+const input = require('fs').readFileSync(0).toString().trim().split('\n');
+
+for (let i = 0; i < input.length; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    
+    if (A === 0 && B === 0) {
+        break; 
+    }
+    
+    console.log(A + B);
+}

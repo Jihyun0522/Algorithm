69ae1e015abfe10eddd58ad888b082ed3db0e64e
Jihyun <wnlemforhs24@gmail.com>
2026-03-08 18:43:34 +0900

    [Bronze II] Title: 분해합, Time: 184 ms, Memory: 9760 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync(0).toString().trim();
+const N = Number(input);
+let result = 0;
+
+for (let i = 1; i < N; i++) {
+    let sum = i;
+    let temp = i;
+    while (temp > 0) {
+        sum += temp % 10;
+        temp = Math.floor(temp / 10);
+    }
+    
+    if (sum === N) {
+        result =  i;
+        break;
+    }
+}
+
+console.log(result);

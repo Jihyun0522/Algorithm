58b9ea26dbd9dfadd3482eebe31aad285bd0d6f5
Jihyun <wnlemforhs24@gmail.com>
2026-03-13 19:29:51 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+people = []
+
+for _ in range(N):
+    weight, height = map(int, input().split())
+    people.append((weight, height))
+
+for i in range(N):
+    rank = 1 
+    for j in range(N):
+        if people[j][0] > people[i][0] and people[j][1] > people[i][1]:
+            rank += 1
+    
+    print(rank, end=' ')

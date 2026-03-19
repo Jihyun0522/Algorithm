f653377fe823cd8071daf7a19ef3b890afd54fe7
Jihyun <wnlemforhs24@gmail.com>
2026-03-19 11:20:22 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync(0).toString();
+
+if (input.includes('w')) {
+    console.log("chunbae");
+} else if (input.includes('b')) {
+    console.log("nabi");
+} else if (input.includes('g')) {
+    console.log("yeongcheol");
+}

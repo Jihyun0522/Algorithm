785e2df9fe9ef282baf692defe3b586fa9cec9d4
Jihyun <wnlemforhs24@gmail.com>
2026-03-23 09:12:17 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 17472 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync(0).toString().trim();
+const N = Number(input);
+
+const dp = new Array(N + 1).fill(0);
+dp[1] = 0;
+
+for (let i = 2; i < N + 1; i++) {
+    dp[i] = dp[i-1] + 1
+    if (i % 2 === 0) {
+        dp[i] = Math.min(dp[i], dp[Math.floor(i / 2)] + 1);
+    }
+    if (i % 3 === 0) {
+        dp[i] = Math.min(dp[i], dp[Math.floor(i / 3)] + 1);
+    }
+}
+
+console.log(dp[N]);

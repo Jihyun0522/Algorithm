39e31177bb0f362aee1d04992f0426cae49d0c0f
Jihyun <wnlemforhs24@gmail.com>
2026-03-23 09:11:54 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 196 ms, Memory: 48452 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+passwords = {}
+for _ in range(N):
+    site, password = input().split()
+    passwords[site] = password
+
+for _ in range(M):
+    query = input().strip()
+    print(passwords[query])

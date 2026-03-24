f541c958157ed841a5d12011820cfaf3443f6805
Jihyun <wnlemforhs24@gmail.com>
2026-03-24 10:21:34 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 92 ms, Memory: 9368 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,22 @@
+const input = require('fs').readFileSync(0).toString().trim().split('\n').map(Number);
+const T = input.shift();
+
+for (let i = 0; i < input.length; i++) {
+    const n = input[i];
+    
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    
+    if (n >= 2) {
+        dp[2] = 2;
+    }
+    if (n >= 3) {
+        dp[3] = 4;
+    }
+    
+    for (let j = 4; j <= n; j++) {
+        dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
+    }
+    
+    console.log(dp[n]);
+}

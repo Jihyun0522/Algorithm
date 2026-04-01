1e2f9fab5f9de0e7ccdfbad6cc82d9eaa7f6ce9a
Jihyun <wnlemforhs24@gmail.com>
2026-04-01 10:50:10 +0900

    [Bronze IV] Title: Winning Score, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,9 @@
+apple = int(input())*3 + int(input())*2 + int(input())
+banana = int(input())*3 + int(input())*2 + int(input())
+
+if apple > banana:
+    print('A')
+elif apple < banana:
+    print('B')
+else:
+    print('T')
